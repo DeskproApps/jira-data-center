faa86e27280dc51a0c80765ca16fe406ee7b60cd
ilia makarov <ilia.makarov@me.com>
2023-12-04 12:52:12 +0200

    Closed ability to create/edit issues

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -43,7 +43,7 @@ export const View: FC<ViewProps> = ({ issueKey }: ViewProps) => {
 
   useEffect(() => {
     client?.registerElement("home", { type: "home_button" });
-    client?.registerElement("edit", { type: "edit_button", payload: issueKey });
+    // client?.registerElement("edit", { type: "edit_button", payload: issueKey });
     client?.deregisterElement("homeContextMenu");
     client?.registerElement("viewContextMenu", { type: "menu", items: [
       { title: "Unlink Ticket", payload: { action: "unlink", issueKey }, },
