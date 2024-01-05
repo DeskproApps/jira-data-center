1660385fd808b027e63978e45e28b6665b4b0ed7
ilia makarov <ilia.makarov@me.com>
2024-01-05 19:47:28 +0200

    Migrate to ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import {DeskproAppProvider} from "@deskpro/app-sdk";
-import { StoreProvider } from "./context/StoreProvider/StoreProvider";
 import { Main } from "./pages/Main";
 
-import "flatpickr/dist/themes/light.css";
-import "tippy.js/dist/tippy.css";
-import "simplebar/dist/simplebar.min.css";
-
-import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
-import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
-
-import TimeAgo from "javascript-time-ago";
-import en from "javascript-time-ago/locale/en.json";
-
-TimeAgo.addDefaultLocale(en)
-
-function App() {
+const App = () => {
   return (
-    <DeskproAppProvider>
-      <StoreProvider>
-        <Main />
-      </StoreProvider>
-    </DeskproAppProvider>
+    <Main />
   );
 }
 
-export default App;
+export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { DeskproAppProvider } from "@deskpro/app-sdk";
+import { StoreProvider } from "./context/StoreProvider/StoreProvider";
+import { App } from "./App";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import "iframe-resizer/js/iframeResizer.contentWindow.js";
+import "flatpickr/dist/themes/light.css";
+import "tippy.js/dist/tippy.css";
+import "simplebar/dist/simplebar.min.css";
+import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
+import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
+
+TimeAgo.addDefaultLocale(en)
+
+const root = ReactDOM.createRoot(document.getElementById("root") as Element);
+root.render((
+  <StrictMode>
+    <DeskproAppProvider>
+      <HashRouter>
+        <StoreProvider>
+          <App />
+        </StoreProvider>
+      </HashRouter>
+    </DeskproAppProvider>
+  </StrictMode>
+));
